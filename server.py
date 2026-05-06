a2a9f1ac3d0ca05c02c60bb96fe2248d89d61227
deivomaciel <deividemaciel2@gmail.com>
2026-05-06 11:53:28 -0300

    first commit

--- server.py
+++ server.py
@@ -5,8 +5,6 @@ import asyncio
 database = Database()
 
 async def start():
-    await database.connect('postgres://neondb_owner:npg_EvI0cnqe5uGw@ep-dry-water-an4grr0p-pooler.c-6.us-east-1.aws.neon.tech/MeetS')
-    user = await UserRepository().createUser('teste', 'addadsadasda', '12345678')
-    print(user)
+    pass
 
 asyncio.run(start())
